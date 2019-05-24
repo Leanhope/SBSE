6bf31fdfd5e30931741010621c240024cec3ced2
Hans <hans.lienhop@gmail.com>
2019-05-24 11:13:14 +0200

    task 3 done, beautified 2.

--- Ex2/Tasks/linearRecomb.py
+++ Ex2/Tasks/linearRecomb.py
@@ -1,25 +1,32 @@
 import numpy as np
 import random
 
-p = 0.25
-x = [random.random()*5 for _ in range(6)]
-v = [random.random()*5 for _ in range(6)]
-alpha = np.random.uniform(-p, 1+p)
-beta = np.random.uniform(-p, 1+p)
+def main():
 
-print("alpha: ", alpha)
-print("beta ", beta)
-print("X: ", x)
-print("V: ", v)
+	p = 0.25
+	n= 6
+	x = [round(random.random()*5, 2) for _ in range(n)]
+	v = [round(random.random()*5, 2) for _ in range(n)]
+	alpha = round(np.random.uniform(-p, 1+p), 2)
+	beta = round(np.random.uniform(-p, 1+p), 2)
 
-for i in range(0, len(x)):
-	t = alpha*x[i] + (1-alpha)*v[i]
-	s = beta*v[i] + (1-beta)*x[i]
+	print("alpha: ", alpha)
+	print("beta ", beta)
+	print("X: ", x)
+	print("V: ", v)
 
-	#Sind die bounds richtig?
-	if min(t, s) >= (min(x[i], v[i])-p) and max(t, s) <= (max(x[i], v[i])+p):
-		x[i] = t
-		v[i] = s
-		
-print("X': ",x)
-print("V': ",v)
+	for i in range(0, len(x)):
+		t = round(alpha*x[i] + (1-alpha)*v[i], 2)
+		s = round(beta*v[i] + (1-beta)*x[i], 2)
+
+		#Sind die bounds richtig?
+		if min(t, s) >= (min(x[i], v[i])-p) and max(t, s) <= (max(x[i], v[i])+p):
+			x[i] = t
+			v[i] = s
+			
+	print("X': ",x)
+	print("V': ",v)
+
+if __name__ == '__main__':
+    main()
+    
--- Ex2/Tasks/sus.py
+++ Ex2/Tasks/sus.py
@@ -1,9 +1,37 @@
 import numpy as np
 import random
 
-x = [_ for _ in range(6)]
-v = [random.random()*100 for _ in range(6)]
-s = sum(v)
-print("X: ", x)
-print("V: ", v)
-print(s)
+def main():
+
+	n = 10
+
+	x = [_ for _ in range(n)]
+	v = [round(random.random()*100, 2) for _ in range(n)]
+
+	for i in range(1, len(v)):
+		v[i] = round(v[i - 1] + v[i], 2)
+
+	p = []
+
+	index = 0
+	counter = 0
+	s = v[len(v)-1]
+	print("X: ", x)
+	print("V: ", v)
+	print("Sum: ", s)
+	offset = round((random.random() * 100) % (s/len(x)), 2)
+	print("Offset: ", offset)
+	print("StepSize: ", round((s/len(x)), 2))
+
+	points = [round(offset + i * (s/len(x)), 2) for i in range(len(x))]
+
+	print("Points: ", points)
+
+	for point in points:
+		while v[index] < point:
+			index = index + 1
+		p.append(x[index])
+	print("Chosen parents: ", p)
+
+if __name__ == '__main__':
+    main()
